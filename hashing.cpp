5dba8b31640fe15f018b74b10d80fbc35b5ca9f3
Adyasha <2003adyasha@gmail.com>
2022-08-17 22:27:52 +0530

    Add polynomial rolling hash function

--- hashing.cpp
+++ hashing.cpp
@@ -2,9 +2,9 @@
 
 int hash_string(string s) {
     // implement hashing function here
-    int m = 7,sum = 0;
+    int m = 7,sum = 0,p=97;
     for(int i = 0;i< s.size();i++){
-        sum = (sum + s[i])%m;
+        sum = (sum*p + s[i])%m;
     }
     int hash = sum%m;
     return hash;
