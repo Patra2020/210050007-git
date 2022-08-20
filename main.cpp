34bde26df11bcf797da1facfbc09a2f295dce992
Adyasha <2003adyasha@gmail.com>
2022-08-21 00:37:04 +0530

    Commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+FIXME
 #include <iostream>
 #include <string>
 #include "hashing.hpp"
@@ -7,5 +8,5 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << hash_string(argv[1]) << endl;
-// FIXME   
+ 
 }
